cf0ff92fa12d15acddd4980b65c55cd7719caf85
William Valadez <billy.valadez@icloud.com>
2023-04-12 19:14:12 -0400

    fixed styles

--- static/animate.js
+++ static/animate.js
@@ -1,25 +1,13 @@
 const wittyString =
-  "Stay street smart with CrimeScope! Quick access to crime data and safety tips to help you travel confidently.";
+  "Stay safe with CrimeScope! Quick access to crime data and safety tips to keep you aware.";
 
 const seriousString =
-  "Stay safe with ease. CrimeScope presents you with real-time crime data and tailored safety tips to help you navigate your city with confidence.";
-
-const catchyString =
-  "Stay safe, stay sharp, stay smart! CrimeScope helps you make informed decisions about your safety with just a tap.";
-
-const funnyString =
-  "Be smarter than the criminals (or at least pretend to be)! CrimeScope gives you the lowdown on crime hotspots and safety tips.";
+  "Travel confidently. CrimeScope presents tailored safety tips to help you navigate cities with confidence.";
 
 const newToCityString =
-  "New to a city? CrimeScope is provides crime data and safety tips to keep you protected.";
+  "New to a city? CrimeScope provides crime data and safety tips to make you feel at home.";
 
-const slides = [
-  wittyString,
-  seriousString,
-  catchyString,
-  funnyString,
-  newToCityString,
-];
+const slides = [wittyString, seriousString, newToCityString];
 
 let currentSlide = 0;
 
@@ -30,6 +18,6 @@ function showNextSlide() {
   });
 }
 
-$("#phrase-container").text(slides[0]);
+$("#phrase-container").text(slides[1]);
 
-setInterval(showNextSlide, 5000);
+setInterval(showNextSlide, 6000);
