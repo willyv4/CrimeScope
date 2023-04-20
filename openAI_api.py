b9bce5caf4ac0a293fb15966c87a703d5d60e3d8
William Valadez <billy.valadez@icloud.com>
2023-04-20 04:38:45 -0400

    making ai repsonse quicker

--- openAI_api.py
+++ openAI_api.py
@@ -22,34 +22,12 @@ def generate_ai_response(crime_data, city):
         property_crime_list.append(
             f"{crime_name} index of {crime_value} compared to the national average of {national_crime}")
 
-    prompt = f"""
-    Crime data for the city of {city}.
-
-    Violent Crimes:
-    {violent_crime_list}.
-
-    Property Crimes:
-    {property_crime_list}.
-
-    Explain the summary of violent and property crimes for {city}'s crime. Provide percentages of the differences between the national and {city}'s crime rates so a 10 year old could understand.
-
-    Share the three most dangerous areas within {city} and explain why they are dangerous.
-
-    Provide safety tips specific to the most common crime rates within {city} and its most dangerous areas. Explain how these safety tips relate to the most common crime rates.
-
-    format your response using tailwind css (full width with no backgound color) and html like this:
-
-    summary 
-
-    most dangerous areas and specific streets to stay away from
-
-    safety tips specific to area and crime (numbered)
-    """
+    prompt = f"""Crime data for {city}: violent crimes include {violent_crime_list}, and property crimes include {property_crime_list}. Please provide an easy-to-understand summary of the differences between national and {city}'s crime rates. Also, please share the three most dangerous areas within {city}, explain why they are dangerous, and provide safety tips related to the most common crime rates in those areas. Please use Tailwind CSS and HTML to format your response (full width, no background color)."""
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        temperature=.5,
-        max_tokens=750,
+        temperature=.4,
+        max_tokens=600,
         messages=[
             {"role": "user", "content": prompt}
         ]
