75999080596e3d4bf2d86caecb6d5e87b60c4c7b
William Valadez <billy.valadez@icloud.com>
2023-04-20 04:45:53 -0400

    working on ai response

--- openAI_api.py
+++ openAI_api.py
@@ -22,12 +22,16 @@ def generate_ai_response(crime_data, city):
         property_crime_list.append(
             f"{crime_name} index of {crime_value} compared to the national average of {national_crime}")
 
-    prompt = f"""Crime data for {city}: violent crimes include {violent_crime_list}, and property crimes include {property_crime_list}. Please provide an easy-to-understand summary of the differences between national and {city}'s crime rates. Also, please share the three most dangerous areas within {city}, explain why they are dangerous, and provide safety tips related to the most common crime rates in those areas. Please use Tailwind CSS and HTML to format your response (full width, no background color)."""
+    prompt = f"""Crime data for {city}: violent crimes include {violent_crime_list}, and property crimes include {property_crime_list}.
+    Provide an easy-to-understand summary of the differences between national and {city}'s crime rates.
+    Also, please share the three most dangerous areas within {city}, explain why they are dangerous, and provide safety tips related to the most common crime rates in those areas.
+    Use Tailwind CSS and HTML to format your response(full width, no background color) like this Summary, Dangerous Areas, Safety Tips.
+    """
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature=.4,
-        max_tokens=600,
+        max_tokens=650,
         messages=[
             {"role": "user", "content": prompt}
         ]
