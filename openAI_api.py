9691ab6c9527bb9c80524418ddc28e2264d1865f
William Valadez <billy.valadez@icloud.com>
2023-05-24 18:25:31 -0400

    added job to ping ai resp

--- openAI_api.py
+++ openAI_api.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import time
 
 openai.api_key = os.getenv("openai_api_key")
 
@@ -12,7 +13,7 @@ def generate_ai_response(v_crime, p_crime, city):
     Use this Crime data {city} for your response: violent crimes include {v_crime}, and property crimes include {p_crime}.
 
     Create a response based on the following:
-    
+
     - Share the 3 most dangerous areas within {city} and explain why they are dangerous, and provide a safety tip accordingly.
     """
 
@@ -30,3 +31,20 @@ def generate_ai_response(v_crime, p_crime, city):
     ai_resp = response.choices[0].message.content.strip()
 
     return ai_resp
+
+
+def job():
+    try:
+        ai_resp = generate_ai_response("0", "0", "ivins, ut")
+    # Do something with the response if needed
+    except Exception as e:
+        # Handle the error or perform any necessary actions
+        print(f"Error occurred in generate_ai_response: {e}")
+
+    # Schedule the next execution after 2.5 hours
+    time.sleep(2.5 * 60 * 60)
+    job()
+
+
+# Start the initial execution
+job()
