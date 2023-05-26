b70fbbc88e02d0dc6db35d066ce5ba4655241292
William Valadez <billy.valadez@icloud.com>
2023-05-26 15:40:43 -0400

    deleted api endpoint

--- app.py
+++ app.py
@@ -513,9 +513,7 @@ def generate_ai():
             session.pop("crimes")
             session.pop("city")
         response = jsonify(data=ai_resp)
-        # Replace with your frontend origin
-        response.headers.add('Access-Control-Allow-Origin',
-                             'http://127.0.0.1:5500')
+
         return response
 
     return jsonify({'error': 'Crime data not found in session'})
